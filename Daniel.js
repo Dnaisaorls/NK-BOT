2d1e135704fd997a680ec72d2864ffe8c3b8a781
Dnaisaorls <95393465+Dnaisaorls@users.noreply.github.com>
2021-12-01 23:55:13 -0600

    Daniel.js

---
+++ Daniel.js
@@ -0,0 +1,12 @@
+{
+	"prefijo": "Daniel",
+	"BotControlador": "+994 40 494 80 06@s.whatsapp.net",
+	"CoCreador": "miku_daniel",
+	"VCardX": "+994 40 494 80 06",
+	"VCardY": "+994 40 494 80 06",
+	"MyPais": "54",
+	"NombreBot": "MIKU-BOT",
+	"LimiteEnGrupos": "999999999",
+	"LimiteComandos": "6669999999999",
+	"TusRedesSociales": "~>[daniel dinex]\n~>[miku bot]\n~>[a]"
+}
